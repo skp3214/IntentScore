e5a270ef04ca557bb1c577ff0975cca4838136a3
Sachin Prajapati <skprajapati3214@gmail.com>
2025-09-25 12:59:02 +0530

    Add URL routing for IntentScoreAPI with product offer and lead upload endpoints for testing in postman

---
+++ IntentScoreAPI/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from IntentScoreAPI import views
+
+urlpatterns = [
+    path('product/offer/', views.create_offer, name='create-offer'),
+    path('leads/upload/', views.upload_leads, name='upload-leads'),
+]
