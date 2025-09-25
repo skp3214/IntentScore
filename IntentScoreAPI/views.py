0f7d58775a8baa7c1cbf0b9d4ec69bec7b554dc9
Sachin Prajapati <skprajapati3214@gmail.com>
2025-09-25 11:41:39 +0530

    Implement create_offer view for handling ProductOffer submissions

--- IntentScoreAPI/views.py
+++ IntentScoreAPI/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProductOfferSerializer
+
+@api_view(['POST'])
+def create_offer(request):
+    serializer = ProductOfferSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
